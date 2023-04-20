714e0b1029ec21bd076657ae4d859f4fafe5cc08
Tomasz <udemytutorialpython@gmail.com>
2023-04-20 15:06:22 +0200

    1. Task page upgraded a. new layout 2. Added task page progress bar functionalities: a. change of precentage width b. change of percentage label
    To do:
    1. Finish task page
    2. Add login authentication
    3. Make nav buttons work
    4. Create functionalities to finished tasks:
    b. change 'on time' attribute
    6. Add authors of tasks

--- main.py
+++ main.py
@@ -42,9 +42,26 @@ class Task(db.Model, Base):
     finish_date = db.Column(db.String(50), nullable=False)
     ontime = db.Column(db.Boolean, nullable=False)
 
+class Subtask(db.Model, Base):
+    __tablename__ = 'Subtasks'
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey('Tasks.id'))
+    text = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    finished = db.Column(db.Boolean, nullable=False)
+    main_task = relationship('Task', backref='subtask')
+
 
 with app.app_context():
     db.create_all()
+
+
+def check_approval(task_id):
+    orig_task = Task.query.get(task_id)
+    if not (Subtask.query.filter_by(task_id=task_id, status='In progress').all() or Subtask.query.filter_by(
+            task_id=task_id, status='Not started').all()):
+        if Subtask.query.filter_by(task_id=task_id, status='Finished').all():
+            orig_task.status = 'Waiting for approval'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -55,7 +72,7 @@ def get_all_tasks():
             description='add task descpription',
             priority=0,
             deadline='not assigned',
-            status='not started'
+            status='Not started'
         )
         db.session.add(task)
         db.session.commit()
@@ -72,6 +89,34 @@ def delete_task(task_id):
     return redirect(url_for('get_all_tasks'))
 
 
+@app.route("/delete/<int:subtask_id>&<int:task_id>")
+def delete_subtask(subtask_id, task_id):
+    subtask_to_delete = Subtask.query.get(subtask_id)
+    db.session.delete(subtask_to_delete)
+
+    check_approval(task_id)
+
+    db.session.commit()
+    return redirect(url_for('show_task', task_id=task_id))
+
+
+@app.route("/mark_as_progress/<int:subtask_id>&<int:task_id>")
+def mark_as_progress(subtask_id, task_id):
+    subtask = Subtask.query.get(subtask_id)
+    orig_task = Task.query.get(task_id)
+    if subtask.status != 'Not started':
+        subtask.status = 'Not started'
+    else:
+        subtask.status = 'In progress'
+    if Subtask.query.filter_by(task_id=task_id, status='In progress').all() or (Subtask.query.filter_by(task_id=task_id, status='Finished').all() and Subtask.query.filter_by(task_id=task_id, status='Not started')):
+        orig_task.status = 'In progress'
+    else:
+        orig_task.status = 'Not started'
+
+    db.session.commit()
+    return redirect(url_for('show_task', task_id=task_id))
+
+
 @app.route("/done/<int:task_id>")
 def move_to_done(task_id):
     task = Task.query.get(task_id)
@@ -79,7 +124,7 @@ def move_to_done(task_id):
     finish_date = now.strftime("%m/%d/%Y")
 
     task.finish_date = finish_date
-    task.status = 'finished'
+    task.status = 'Finished'
     task.finished = True
     task.ontime = True
 
@@ -87,6 +132,24 @@ def move_to_done(task_id):
     done_list.append(Task.query.get(task_id))
 
     return redirect(url_for('get_all_tasks'))
+
+@app.route("/done/<int:subtask_id>&<int:task_id>")
+def move_subtask_to_done(subtask_id, task_id):
+    subtask = Subtask.query.get(subtask_id)
+
+    now = datetime.datetime.now()
+    finish_date = now.strftime("%m/%d/%Y")
+
+    subtask.finish_date = finish_date
+    subtask.status = 'Finished'
+    subtask.finished = True
+
+    check_approval(task_id)
+
+    db.session.commit()
+
+    return redirect(url_for('show_task', task_id=task_id))
+
 
 @app.route("/new-task", methods=['GET', 'POST'])
 def new_task():
@@ -101,7 +164,7 @@ def new_task():
             text=request.form['title'],
             description=description,
             priority=request.form['priority'],
-            status='not started',
+            status='Not started',
             deadline=request.form['deadline'],
             finished=False,
             start_date=start_date,
@@ -110,15 +173,37 @@ def new_task():
         )
         db.session.add(task)
         db.session.commit()
-        tasks = Task.query.all()
         return redirect(url_for('get_all_tasks'))
     return render_template("new-task.html")
 
 
-@app.route('/task/<int:task_id>')
+@app.route('/task/<int:task_id>', methods=['GET', 'POST'])
 def show_task(task_id):
-    print('tutej')
-    return render_template('task.html')
+    task = Task.query.get(task_id)
+    if request.method == 'POST':
+        subtask = Subtask(
+            text=request.form['task'],
+            status='Not startedl',
+            finished=False,
+            main_task=task
+        )
+        db.session.add(subtask)
+        db.session.commit()
+        mark_as_progress(subtask_id=subtask.id, task_id=task_id)
+
+    subtasks = Subtask.query.filter_by(task_id=task_id, finished=False).all()
+    finished_subtasks = Subtask.query.filter_by(task_id=task_id, finished=True).all()
+    all_subtasks = Subtask.query.filter_by(task_id=task_id).all()
+    len_all = len(all_subtasks)
+    print(len_all)
+    len_finished = len(finished_subtasks)
+    try:
+        bar_percentage = int(len_finished/len_all*100)
+    except ZeroDivisionError:
+        bar_percentage = 0
+    print(bar_percentage)
+
+    return render_template('task.html', task=task, subtasks=subtasks, finished_subtasks=finished_subtasks, percentage=bar_percentage)
 
 
 
